66c02e7750a2ec13d8799bd2963d02154c7d805d
penglitop <pengli_top@163.com>
2017-12-05 14:53:05 +0800

    部分JS效果添加五

--- public/javascripts/list_page/list_product.js
+++ public/javascripts/list_page/list_product.js
@@ -29,24 +29,112 @@
 /*列表出现 结束*/
 
 /*综合-销量-价格-筛选 开始*/
-;(function(gl4bal){
-	var newChange=bycssAll('#new_change'),
-		activeInd=0;	
+;(function(global){
+	var saleBtn=bycss('#sales'),
+		fliterBtn=bycss('#fliter'),
+		multiBtn=bycss('#multiple'),
+		priceBtn=bycss('#price'),
+		that=multiBtn,
+		activeInd=0,
+		sortStatus='';
 
-	for(var i=0;i<newChange.length;i++){
-		newChange[i].index=i;
-		newChange[i].addEventListener('click',function(){
-			if(!hasClass(this,'active')){
-				if(this.index==0){
-					removeClass(newChange[activeInd],'active');
-					addClass(this,'active');
-				}else if(this.index==1){
-					removeClass(newChange[activeInd],'active');
-					addClass(this,'active');
-					
-				}
+	var multiBtn=bycss('#multiple'),
+		coverFloor=bycss('#cover_floor'),
+		searchSort18=bycss('#searchSort18'),
+		arrow=byEl('i',searchSort18),
+		search1=bycss('#search1'),
+		activeInd=0,
+		sortofBrand=bycssAll('.sort_of_brand'),
+		sidebarItem=bycssAll('.sidebar_iteam');
+
+	var int=function(){
+		saleBtn.addEventListener('click',sales);
+		fliterBtn.addEventListener('click',fliter);
+		priceBtn.addEventListener('click',price);
+		for(var i=0;i<sidebarItem.length;i++){
+			sidebarItem[i].index=i;
+			sidebarItem[i].addEventListener('click',multipleItem);
+		}
+	},
+
+	sales=function(){
+		if(that){
+			removeClass(that,'active');
+			if(that==priceBtn){
+				removeClass(that,'downPrice');
+				removeClass(that,'upPrice');
 			}
-		})
-	}
+		}
+		addClass(this,'active');
+		that=this;
+	},
+
+	multiple=function(){
+		if(searchSort18.style.display=='none'){
+			show(coverFloor);
+			show(searchSort18);
+			addClass(search1,'space_height');
+		}else{
+			hide(coverFloor);
+			hide(searchSort18);
+			removeClass(search1,'space_height');
+		}
+	},
+
+	multipleItem=function(){
+		if(!hasClass(this,'active')){
+			removeClass(sidebarItem[activeInd],'active');
+			removeClass(arrow[activeInd],'tick');
+			addClass(this,'active');
+			addClass(arrow[this.index],'tick');
+			multiBtn.innerHTML=sortofBrand[this.index].innerHTML;
+			addClass(multiBtn,'active');
+			hide(coverFloor);
+			hide(searchSort18);
+			removeClass(search1,'space_height');
+			activeInd=this.index;
+		}
+	},
+
+	price=function(){
+		if(that){
+			removeClass(that,'active');
+			if(that==priceBtn){
+				removeClass(that,'downPrice');
+				removeClass(that,'upPrice');
+			}
+		}
+		addClass(this,'active');
+		addClass(this,'upPrice');
+		that=this;
+	};
+
+	int();
+
+	
+
+	multiBtn.addEventListener('click',function(){
+		multiple();
+
+		if(that){
+			if(that==multiBtn){
+				return;
+			}
+			if(that==priceBtn){
+				removeClass(that,'downPrice');
+				removeClass(that,'upPrice');
+			}
+			removeClass(that,'active');
+		}
+		that=this;
+			
+		});	
 })(this);
 /*综合-销量-价格-筛选 结束*/
+
+/*弹窗筛选 开始*/
+;(function(global){
+
+
+})(this);
+/*弹窗筛选 结束*/
